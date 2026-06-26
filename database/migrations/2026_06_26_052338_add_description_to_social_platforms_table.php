9ee3f982f56464db44bf2fc4dad32a63d4d10a5c
ahmed <alfker3@gmail.com>
2026-06-26 08:26:11 +0300

    fix table name

--- database/migrations/2026_06_26_052338_add_description_to_social_platforms_table.php
+++ database/migrations/2026_06_26_052338_add_description_to_social_platforms_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('social', function (Blueprint $table) {
+        Schema::table('social_platforms', function (Blueprint $table) {
             $table->text('description')->nullable()->after('name');
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('social', function (Blueprint $table) {
+        Schema::table('social_platforms', function (Blueprint $table) {
             $table->dropColumn('description');
         });
     }
